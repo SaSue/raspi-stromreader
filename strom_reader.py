c65958297302b9b4d5e67da55629012e992d77ea
SaSue <sandro@suechting.com>
2025-04-11 21:10:45 +0200

    Update strom_reader.py

--- strom_reader.py
+++ strom_reader.py
@@ -5,17 +5,23 @@ import logging
 import binascii
 import crcmod
 from datetime import datetime
+import argparse
+
+# Argumente parsen
+parser = argparse.ArgumentParser(description="SML-Reader mit optionalem Debug")
+parser.add_argument("--debug", action="store_true", help="Debug-Modus aktivieren")
+args = parser.parse_args()
 
 PORT = "/dev/ttyUSB0"
 BAUDRATE = 9600
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG if args.debug else logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
-logging.info("🔌 Verbinde mit %s @ %d Baud", PORT, BAUDRATE)
+logging.debug("🔌 Verbinde mit %s @ %d Baud", PORT, BAUDRATE)
 
 ser = serial.Serial(PORT, BAUDRATE, timeout=1)
 
@@ -44,13 +50,13 @@ while True:
 
       #  crc_calculated = binascii.crc_hqx(sml_data, 0xffff)
         crc_calculated = crc_func(sml_data)
-        logging.info("")
-        logging.info("[%s]", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        logging.info("📡 SML-Telegramm erkannt (Länge: %d Bytes)", len(sml_data))
-        logging.info("🔢 HEX: %s", sml_komplett.hex())
-        logging.info("🔢 HEX: %s", sml_data.hex())
-        logging.info("🔢 CRC-Rohbytes: %s", crc_raw.hex())
-        logging.info("✅ CRC: erwartet %04X, berechnet %04X → %s",
+        logging.debug("")
+        logging.debug("[%s]", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        logging.debug("📡 SML-Telegramm erkannt (Länge: %d Bytes)", len(sml_data))
+        logging.debug("🔢 HEX: %s", sml_komplett.hex())
+        logging.debug("🔢 HEX: %s", sml_data.hex())
+        logging.debug("🔢 CRC-Rohbytes: %s", crc_raw.hex())
+        logging.debug("✅ CRC: erwartet %04X, berechnet %04X → %s",
                      crc_expected,
                      crc_calculated,
                      "✅ gültig" if crc_expected == crc_calculated else "❌ ungültig")
