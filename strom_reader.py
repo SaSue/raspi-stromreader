15353f7dfe48bfd06a816795ce273cce5f755623
SaSue <sandro@suechting.com>
2025-04-11 19:00:26 +0200

    Update strom_reader.py

--- strom_reader.py
+++ strom_reader.py
@@ -42,7 +42,7 @@ while True:
                 continue
 
             # Enthält: [START]...1b1b1b1b1a[FILLBYTE][CRC1][CRC2]
-            telegram_body = buffer[start_idx:end_idx + 8 ]  # Endbyt, Fillbye CRCx2
+            telegram_body = buffer[start_idx:end_idx + 11 ]  # Endbyt, Fillbye CRCx2
             fill_byte = buffer[end_idx + 2:end_idx + 3]
             logging.info("🔢 HEX raw: %s", telegram_body.hex())
             crc_raw = buffer[end_idx + 3:end_idx + 5]
