1f7e4d088f0f8bb722b94c24c8d0554365dbd544
SaSue <sandro@suechting.com>
2025-04-14 10:51:42 +0200

    debugging

--- data-migration/data-migration.py
+++ data-migration/data-migration.py
@@ -18,7 +18,7 @@ with open(JSON_DATEI, "r", encoding="utf-8") as f:
 i =0
 # Daten einfügen
 for eintrag in daten:
-"""
+    """
     # Messwert einfügen
     cursor.execute("""
         INSERT INTO messwerte (zaehler_id, timestamp, bezug_kwh, einspeisung_kwh, wirkleistung_watt)
