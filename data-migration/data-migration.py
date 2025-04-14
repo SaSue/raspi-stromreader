bd37c0ad41cc730f094a0168b67db731fdddda2d
SaSue <sandro@suechting.com>
2025-04-14 10:56:59 +0200

    debug

--- data-migration/data-migration.py
+++ data-migration/data-migration.py
@@ -10,30 +10,14 @@ DB_DATEI = "/app/data/strom.sqlite"
 conn = sqlite3.connect(DB_DATEI)
 cursor = conn.cursor()
 
-zaehler_id = 1  # ID des Zählers    
-
 # JSON-Datei einlesen
 with open(JSON_DATEI, "r", encoding="utf-8") as f:
     daten = json.load(f)
-i =0
-# Daten einfügen
-for eintrag in daten:
-    
-    # Messwert einfügen
-    #cursor.execute("""
-    #    INSERT INTO messwerte (zaehler_id, timestamp, bezug_kwh, einspeisung_kwh, wirkleistung_watt)
-    #    VALUES (?, ?, ?, ?, ?)
-    #""", (
-    #    zaehler_id,
-    #    eintrag["timestamp"],
-    #    eintrag["bezug"],
-    #    eintrag["einspeisung"],
-    #    eintrag["leistung"]
-    #))
-  
-    print(f"Schleifendurchlauf {i} Messwert: {eintrag}")  
-    i += 1
 
+# Daten einfügen
+for i, eintrag in enumerate(daten):  # Verwende enumerate, um den Index automatisch zu zählen
+    # Messwert einfügen
+    print(f"Schleifendurchlauf {i} Messwert: {eintrag}")
 
 # Änderungen speichern und Verbindung schließen
 conn.commit()
